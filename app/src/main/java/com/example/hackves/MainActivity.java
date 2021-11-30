92c8c25d814cc4c85cba0ff5c0a9515d94cb1e90
bhavin121 <bhavin7014316242@gmail.com>
2021-11-30 11:49:14 +0530

    First Commit

--- app/src/main/java/com/example/hackves/MainActivity.java
+++ app/src/main/java/com/example/hackves/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.hackves;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,14 +20,19 @@ import android.widget.MediaController;
 import android.widget.Toast;
 
 import com.example.hackves.databinding.ActivityMainBinding;
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TYPE_AUDIO = MediaStore.Audio.Media.DATA;
     public static final String TYPE_VIDEO = MediaStore.Video.Media.DATA;
+    public static final int REQUEST_CODE = 100;
 
     ActivityMainBinding binding;
     FFmpeg fFmpeg;
@@ -39,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         getVideoLauncher = registerForActivityResult(new ActivityResultContracts.GetContent() , new ActivityResultCallback<Uri>() {
             @Override
             public void onActivityResult(Uri result){
-                System.out.println(result);
+                System.out.println(getFilePath(result, TYPE_VIDEO));
                 binding.video.setVideoURI(result);
                 binding.video.setMediaController(new MediaController(MainActivity.this));
                 binding.video.start();
@@ -54,15 +64,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        binding.pickVideo.setOnClickListener(view -> {
-            pickVideo();
-        });
+        binding.pickVideo.setOnClickListener(view -> pickVideo());
+
         buildMessageDialog();
         initFFmpeg();
     }
 
     private void pickVideo(){
-        getVideoLauncher.launch("video/*");
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==PackageManager.PERMISSION_GRANTED){
+            getVideoLauncher.launch("*/*");
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                    , REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode , @NonNull String[] permissions , @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode , permissions , grantResults);
+
+        if(requestCode == REQUEST_CODE &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED
+        ){
+            getVideoLauncher.launch("video/*");
+        }else{
+            Toast.makeText(MainActivity.this , "Permission not granted" , Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void buildMessageDialog( ){
@@ -73,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 .create();
     }
 
-    private void initFFmpeg( ){
+    private void initFFmpeg(){
         if(fFmpeg == null){
             fFmpeg = FFmpeg.getInstance(this);
             try {
@@ -104,6 +134,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void executeFFmpegCommand(String []cmd){
+        try {
+            fFmpeg.execute(cmd, new ExecuteBinaryResponseHandler(){
+                @Override
+                public void onSuccess(String message){
+                    super.onSuccess(message);
+                }
+
+                @Override
+                public void onFailure(String message){
+                    super.onFailure(message);
+                }
+            });
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void showUnsupportedError(){
         messageDialog.setMessage("App not supported on your device");
         messageDialog.setButton(AlertDialog.BUTTON_POSITIVE , "Ok" , (dialogInterface , i) -> {
@@ -126,4 +174,5 @@ public class MainActivity extends AppCompatActivity {
 
         return filePath;
     }
+
 }
